18d20cebfdb56a128800d6f8867fa8be03a5dc2f
jacen11 <jacen19931@gmail.com>
2021-04-28 23:09:02 +0300

    логер

--- app/src/main/java/dev/dpastukhov/coinmarketinfo/di/CoinModule.kt
+++ app/src/main/java/dev/dpastukhov/coinmarketinfo/di/CoinModule.kt
@@ -8,14 +8,21 @@ import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.multibindings.IntoMap
+import dev.dpastukhov.coinmarketinfo.BuildConfig
 import dev.dpastukhov.coinmarketinfo.ViewModelFactory
 import dev.dpastukhov.coinmarketinfo.ViewModelKey
 import dev.dpastukhov.coinmarketinfo.data.CoinMarketServiceApi
 import dev.dpastukhov.coinmarketinfo.presentation.CoinListViewModel
+import okhttp3.*
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.create
+import java.io.IOException
 import javax.inject.Provider
 import javax.inject.Singleton
+
+import okhttp3.logging.HttpLoggingInterceptor.Level.BASIC
+import okhttp3.logging.HttpLoggingInterceptor.Level.BODY
 
 @Module
 class CoinModule {
@@ -26,13 +33,19 @@ class CoinModule {
     @Provides
     fun provideGeckoRetrofit(gson: Gson): Retrofit = Retrofit.Builder()
         .baseUrl("https://pro-api.coinmarketcap.com/v1/")
+        .client(getClient())
         .build()
 
     @Provides
     fun provideService(retrofit: Retrofit): CoinMarketServiceApi = retrofit.create()
 
-
-
+    private fun getClient(): OkHttpClient {
+        val logging = HttpLoggingInterceptor()
+        logging.level = if (BuildConfig.DEBUG) BODY else BASIC
+        val okHttp: OkHttpClient.Builder = OkHttpClient.Builder()
+        okHttp.addInterceptor(logging)
+        return okHttp.build()
+    }
 }
 
 @Module
