70257e33bc5bd49422dbaaac307f0e76edf15c82
jacen <jacen19931@gmail.com>
2021-05-02 23:02:34 +0300

    строка

--- app/src/main/java/dev/dpastukhov/coinmarketinfo/presentation/CoinListFragment.kt
+++ app/src/main/java/dev/dpastukhov/coinmarketinfo/presentation/CoinListFragment.kt
@@ -8,8 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.snackbar.Snackbar
-import dagger.internal.DaggerCollections
-import dev.dpastukhov.coinmarketinfo.ViewModelFactory
+import dev.dpastukhov.coinmarketinfo.R
 import dev.dpastukhov.coinmarketinfo.databinding.CoinListFragmentBinding
 import dev.dpastukhov.coinmarketinfo.di.DaggerCoinComponent
 import javax.inject.Inject
@@ -42,7 +41,7 @@ class CoinListFragment : Fragment() {
 
         viewModel.setCoinList()
         viewModel.error.observe(viewLifecycleOwner) {
-            Snackbar.make(binding.root, "error", Snackbar.LENGTH_LONG).show()
+            Snackbar.make(binding.root, getString(R.string.error_text), Snackbar.LENGTH_LONG).show()
         }
 
         return binding.root
